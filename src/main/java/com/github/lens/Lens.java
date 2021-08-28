3539c7a4bc5e2c23c820558162beac6e161c59ae
Khadanovich Sergey <khadanovich.sergey@gmail.com>
2021-08-29 00:21:05 +0300

    Add accessor-based lens impl.

---
+++ src/main/java/com/github/lens/Lens.java
@@ -0,0 +1,57 @@
+package com.github.lens;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+/**
+ * Lens interface.
+ *
+ * <p>To create a simple, accessor based lens use {@link Lens#fromAccessors(Function, BiConsumer)}
+ * factory method.
+ * <p>For example,
+ * <pre> {@code
+ * Lens<Person, String> nameLens =
+ *   Lens.fromAccessors(Person::getName, Person::setName);
+ * }</pre>
+ *
+ * @param <O> object type
+ * @param <P> property type
+ * @author Sergei_Khadanovich
+ */
+public interface Lens<O, P> {
+
+    /**
+     * Get property from object.
+     *
+     * <p>Object value can be {@literal null}. In this case, return value will be {@literal null}.
+     *
+     * @param object object
+     * @return extracted property value
+     */
+    P get(O object);
+
+    /**
+     * Set property into object.
+     *
+     * <p>Object value can be {@literal null}. In this case, property value will not be set.
+     *
+     * @param object   object
+     * @param property property value
+     */
+    void set(O object, P property);
+
+
+    /**
+     * Create a lens from accessors methods.
+     *
+     * @param getter getter accessor
+     * @param setter setter accessor
+     * @param <O>    object type
+     * @param <P>    property type
+     * @return lens
+     */
+    static <O, P> Lens<O, P> fromAccessors(Function<? super O, ? extends P> getter,
+                                           BiConsumer<? super O, ? super P> setter) {
+        return new AccessorsLens<>(getter, setter);
+    }
+}
---
+++ src/test/java/com/github/lens/AccessorsLensTest.java
@@ -0,0 +1,66 @@
+package com.github.lens;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.Data;
+import org.testng.annotations.Test;
+
+/**
+ * @author Sergei_Khadanovich
+ */
+public class AccessorsLensTest {
+
+    private static final Lens<Entity, String> LENS =
+            Lens.fromAccessors(Entity::getProperty, Entity::setProperty);
+
+    @Test
+    public void get_objectIsNull_returnNull() {
+        assertThat(LENS.get(null)).isNull();
+    }
+
+    @Test
+    public void get_objectPropertyIsNull_returnNull() {
+        Entity entity = new Entity();
+
+        assertThat(LENS.get(entity)).isNull();
+    }
+
+    @Test
+    public void get_objectPropertyIsNotNull_returnIt() {
+        Entity entity = new Entity();
+        entity.setProperty("value");
+
+        assertThat(LENS.get(entity)).isEqualTo("value");
+    }
+
+    @Test
+    public void set_objectIsNull_doNotSet() {
+        // this test check, that there is no NullPointerException
+        LENS.set(null, "value");
+    }
+
+    @Test
+    public void set_propertyIsNull_setNull() {
+        Entity entity = new Entity();
+        entity.setProperty("value");
+
+        LENS.set(entity, null);
+
+        assertThat(entity.getProperty()).isNull();
+    }
+
+    @Test
+    public void set_propertyIsNotNull_setNewPropertyValue() {
+        Entity entity = new Entity();
+        entity.setProperty("value");
+
+        LENS.set(entity, "new value");
+
+        assertThat(entity.getProperty()).isEqualTo("new value");
+    }
+
+    @Data
+    static class Entity {
+        String property;
+    }
+}
