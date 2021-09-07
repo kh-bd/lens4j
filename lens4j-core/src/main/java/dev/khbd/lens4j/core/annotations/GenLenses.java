281930db9d3cf990ff9c1c022839bd9a4a704e34
Sergey Khadanovich <khadanovich.sergey@gmail.com>
2021-09-07 15:36:02 +0300

    Add java doc for GenLenses annotation.

--- lens4j-core/src/main/java/dev/khbd/lens4j/core/annotations/GenLenses.java
+++ lens4j-core/src/main/java/dev/khbd/lens4j/core/annotations/GenLenses.java
@@ -7,7 +7,33 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation to generated factory with specified lenses.
+ * Annotation to generate factory with specified lenses.
+ *
+ * <p>Lenses can be generated at compile time. To do this, mark class with
+ * {@link GenLenses} annotation and specify {@link Lens} annotation for
+ * each desired lens.
+ *
+ * <p>For example, if we have a code like this
+ * <pre>{@code @GenLenses(lenses = @Lens(path = "account.accountNumber", lensName = "ACCOUNT_NUMBER_LENS"))
+ * class Payment {
+ *      Account account;
+ *
+ *      ... accessor methods
+ * }
+ * class Account {
+ *      String accountNumber;
+ *
+ *      ... accessor methods
+ * }
+ * }</pre>
+ * {@code PaymentLenses} factory will be generated
+ * <pre>{@code
+ * final class PaymentLenses {
+ *     public static final ReadLens<Payment, String> ACCOUNT_NUMBER_LENS =
+ *         Lenses.readLens(Payment::getAccount)
+ *            .andThen(Lenses.readLens(Account::getAccountNumber));
+ * }
+ * }</pre>
  *
  * @author Sergei_Khadanovich
  */
