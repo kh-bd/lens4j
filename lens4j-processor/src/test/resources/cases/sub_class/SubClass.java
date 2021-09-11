961b2040f0e2a93529303a5fcd22374f6d725e2d
Sergey Khadanovich <khadanovich.sergey@gmail.com>
2021-09-11 13:55:50 +0300

    Properties from base classes can be used in lens path.

---
+++ lens4j-processor/src/test/resources/cases/sub_class/SubClass.java
@@ -0,0 +1,8 @@
+package cases.sub_class;
+
+import dev.khbd.lens4j.core.annotations.GenLenses;
+import dev.khbd.lens4j.core.annotations.Lens;
+
+@GenLenses(lenses = @Lens(path = "currency.code"))
+public class SubClass extends BaseClass {
+}
---
+++ lens4j-processor/src/test/resources/cases/sub_class/SubClassLenses.java
@@ -0,0 +1,15 @@
+package cases.sub_class;
+
+import common.Currency;
+import dev.khbd.lens4j.core.Lenses;
+import dev.khbd.lens4j.core.ReadLens;
+import java.lang.String;
+import javax.annotation.processing.Generated;
+
+@Generated("dev.khbd.lens4j.processor.LensProcessor")
+public final class SubClassLenses {
+    public static final ReadLens<SubClass, String> CURRENCY_CODE = Lenses.readLens(SubClass::getCurrency).andThen(Lenses.readLens(Currency::getCode));
+
+    private SubClassLenses() {
+    }
+}
