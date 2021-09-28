f37d1a8c471969d38aa9377b52d2af768fda3056
Sergey Khadanovich <khadanovich.sergey@gmail.com>
2021-09-28 14:17:29 +0300

    Processor supports array types. (#73)

---
+++ lens4j-processor/src/test/resources/cases/array_type_at_the_end/WithArrayLenses.java
@@ -0,0 +1,21 @@
+package cases.array_type_at_the_end;
+
+import dev.khbd.lens4j.core.Lenses;
+import dev.khbd.lens4j.core.ReadLens;
+import dev.khbd.lens4j.core.ReadWriteLens;
+import java.lang.String;
+import javax.annotation.processing.Generated;
+
+@Generated("dev.khbd.lens4j.processor.LensProcessor")
+public final class WithArrayLenses {
+    public static final ReadLens<WithArray, byte[]> PRIMITIVE_ARRAY_READ_LENS = Lenses.readLens(WithArray::getPrimitiveArray);
+
+    public static final ReadWriteLens<WithArray, byte[]> PRIMITIVE_ARRAY_READ_WRITE_LENS = Lenses.readWriteLens(WithArray::getPrimitiveArray, WithArray::setPrimitiveArray);
+
+    public static final ReadLens<WithArray, String[]> DECLAR_ARRAY_READ_LENS = Lenses.readLens(WithArray::getDeclarArray);
+
+    public static final ReadWriteLens<WithArray, String[]> DECLAR_ARRAY_READ_WRITE_LENS = Lenses.readWriteLens(WithArray::getDeclarArray, WithArray::setDeclarArray);
+
+    private WithArrayLenses() {
+    }
+}
