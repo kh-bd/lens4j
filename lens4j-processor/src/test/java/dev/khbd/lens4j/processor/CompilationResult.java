8dbe066597cf487658c34ae1f2dfa97a2de77786
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2022-10-30 00:16:36 +0700

    Add convenient classes to test generation.

---
+++ lens4j-processor/src/test/java/dev/khbd/lens4j/processor/CompilationResult.java
@@ -0,0 +1,46 @@
+package dev.khbd.lens4j.processor;
+
+import com.google.testing.compile.Compilation;
+import com.google.testing.compile.JavaFileObjects;
+
+import static com.google.testing.compile.CompilationSubject.assertThat;
+
+/**
+ * @author Sergei_Khadanovich
+ */
+public class CompilationResult {
+
+    private final Compilation compilation;
+
+    public CompilationResult(Compilation compilation) {
+        this.compilation = compilation;
+    }
+
+    public CompilationResult success() {
+        assertThat(compilation).succeeded();
+        return this;
+    }
+
+    public CompilationResult generated(String fileName, String path) {
+        assertThat(compilation)
+                .generatedSourceFile(fileName)
+                .hasSourceEquivalentTo(JavaFileObjects.forResource(path));
+        return this;
+    }
+
+    public CompilationResult failed(String... messages) {
+        assertThat(compilation).failed();
+        for (String message : messages) {
+            assertThat(compilation).hadErrorContaining(message);
+        }
+        return this;
+    }
+
+    public CompilationResult warn(String... messages) {
+        for (String message : messages) {
+            assertThat(compilation).hadWarningContaining(message);
+        }
+        return this;
+    }
+
+}
