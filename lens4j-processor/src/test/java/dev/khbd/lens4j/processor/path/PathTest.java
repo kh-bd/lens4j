dc3a8a56980d1d2eb4df293648cbe9a5a3297f22
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2022-11-04 14:03:54 +0700

    Add tests for `Path.isEmpty` method.

--- lens4j-processor/src/test/java/dev/khbd/lens4j/processor/path/PathTest.java
+++ lens4j-processor/src/test/java/dev/khbd/lens4j/processor/path/PathTest.java
@@ -4,12 +4,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 /**
  * @author Sergei_Khadanovich
  */
 public class PathTest {
 
     private final PathParser pathParser = new PathParser();
+
+    @Test
+    public void isEmpty_pathHasNoParts_returnTrue() {
+        Path path = new Path();
+
+        assertThat(path.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void isEmpty_pathHasSomeParts_returnFalse() {
+        Path path = new Path(List.of(new Method("name", 0)));
+
+        assertThat(path.isEmpty()).isFalse();
+    }
 
     @Test(expectedExceptions = IllegalStateException.class,
             expectedExceptionsMessageRegExp = "Path is empty\\. Cannot remove last part")
