9336eab3b4e19ef8b2063a24294a4f6995bad561
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-01-26 23:04:30 +0300

    Several @GenLenses can point to same factory.
    (cherry picked from commit 4aa5aa560b30d8b69abc97835ca5aaf3b00b285d)

--- lens4j-processor/src/main/java/dev/khbd/lens4j/processor/LensProcessor.java
+++ lens4j-processor/src/main/java/dev/khbd/lens4j/processor/LensProcessor.java
@@ -6,6 +6,7 @@ import dev.khbd.lens4j.core.annotations.GenLenses;
 import dev.khbd.lens4j.processor.generator.CombinedLensFactoryGenerator;
 import dev.khbd.lens4j.processor.generator.InlinedLensFactoryGenerator;
 import dev.khbd.lens4j.processor.generator.LensFactoryGenerator;
+import dev.khbd.lens4j.processor.meta.FactoryId;
 import dev.khbd.lens4j.processor.meta.FactoryMeta;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -17,7 +18,10 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Annotation processor for generating lens.
@@ -73,9 +77,13 @@ public class LensProcessor extends AbstractProcessor {
     private ProcessResult processElements(RoundEnvironment roundEnv) {
         Set<? extends Element> lensElements = findLensElements(roundEnv);
         try {
-            for (Element element : lensElements) {
-                FactoryMeta factoryMeta = metaBuilder.build(element);
-                JavaFile file = lensGenerator.generate(factoryMeta);
+            Map<FactoryId, List<FactoryMeta>> groupedFactories =
+                    lensElements.stream()
+                            .map(metaBuilder::build)
+                            .collect(Collectors.groupingBy(FactoryMeta::getId));
+            for (List<FactoryMeta> factories : groupedFactories.values()) {
+                FactoryMeta merged = mergeFactories(factories);
+                JavaFile file = lensGenerator.generate(merged);
                 writeFile(file);
             }
         } catch (LensProcessingException e) {
@@ -83,6 +91,21 @@ public class LensProcessor extends AbstractProcessor {
             return ProcessResult.ERROR;
         }
         return ProcessResult.GENERATED;
+    }
+
+    private FactoryMeta mergeFactories(List<FactoryMeta> factories) {
+        if (factories.size() == 1) {
+            return factories.getFirst();
+        }
+        FactoryMeta merged = factories.getFirst();
+        for(int i = 1; i < factories.size(); i++) {
+            FactoryMeta current = factories.get(i);
+            if (!merged.canBeMergedWith(current)) {
+                throw new LensProcessingException(MessageFactory.factoriesCannotBeMerged(current.getId()));
+            }
+            merged = merged.merge(current);
+        }
+        return merged;
     }
 
     private Set<? extends Element> findLensElements(RoundEnvironment roundEnv) {
