aa38489a2ab81c1562f116ac496f0e159bb382f0
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-01-27 21:36:56 +0300

    Support only inlined lenses.
    (cherry picked from commit 48c8af05833af5d8956b6d38407dd96cfaaff992)

--- lens4j-processor/src/test/resources/cases/method/generic/class_generics/PaymentLenses.java
+++ lens4j-processor/src/test/resources/cases/method/generic/class_generics/PaymentLenses.java
@@ -1,7 +1,7 @@
 package cases.method.generic.class_generics;
 
-import dev.khbd.lens4j.core.Lenses;
 import dev.khbd.lens4j.core.ReadLens;
+import java.lang.Override;
 import java.lang.String;
 import java.lang.UnsupportedOperationException;
 import java.util.List;
@@ -9,9 +9,25 @@ import javax.annotation.processing.Generated;
 
 @Generated("dev.khbd.lens4j.processor.LensProcessor")
 public final class PaymentLenses {
-    public static final ReadLens<Payment, String> FROM_READ_LENS = Lenses.readLens(Payment::from);
+    public static final ReadLens<Payment, String> FROM_READ_LENS = new ReadLens<Payment, String>() {
+        @Override
+        public final String get(Payment object) {
+            if (object == null) {
+                return null;
+            }
+            return object.from();
+        }
+    };
 
-    public static final ReadLens<Payment, List<String>> FROMS_READ_LENS = Lenses.readLens(Payment::froms);
+    public static final ReadLens<Payment, List<String>> FROMS_READ_LENS = new ReadLens<Payment, List<String>>() {
+        @Override
+        public final List<String> get(Payment object) {
+            if (object == null) {
+                return null;
+            }
+            return object.froms();
+        }
+    };
 
     private PaymentLenses() {
         throw new UnsupportedOperationException("Can not create instance of factory class");
--- lens4j-processor/src/test/resources/cases/multi_property/PaymentLenses.java
+++ lens4j-processor/src/test/resources/cases/multi_property/PaymentLenses.java
@@ -3,30 +3,118 @@ package cases.multi_property;
 import common.Bank;
 import common.Payer;
 import common.Receiver;
-import dev.khbd.lens4j.core.Lenses;
 import dev.khbd.lens4j.core.ReadLens;
 import dev.khbd.lens4j.core.ReadWriteLens;
+import java.lang.Override;
 import java.lang.String;
 import java.lang.UnsupportedOperationException;
 import javax.annotation.processing.Generated;
 
 @Generated("dev.khbd.lens4j.processor.LensProcessor")
 public final class PaymentLenses {
-    public static final ReadLens<Payment, String> PAYMENT_RECEIVER_BANK_BIC_READ_LENS = Lenses.readLens(Payment::getReceiver)
-            .andThen(Lenses.readLens(Receiver::getBank))
-            .andThen(Lenses.readLens(Bank::getBic));
+    public static final ReadLens<Payment, String> PAYMENT_RECEIVER_BANK_BIC_READ_LENS = new ReadLens<Payment, String>() {
+        @Override
+        public final String get(Payment object) {
+            if (object == null) {
+                return null;
+            }
+            Receiver receiver = object.getReceiver();
+            if (receiver == null) {
+                return null;
+            }
+            Bank bank = receiver.getBank();
+            if (bank == null) {
+                return null;
+            }
+            return bank.getBic();
+        }
+    };
 
-    public static final ReadWriteLens<Payment, String> PAYMENT_RECEIVER_BANK_BIC_READ_WRITE_LENS = Lenses.readLens(Payment::getReceiver)
-            .andThen(Lenses.readLens(Receiver::getBank))
-            .andThen(Lenses.readWriteLens(Bank::getBic, Bank::setBic));
+    public static final ReadWriteLens<Payment, String> PAYMENT_RECEIVER_BANK_BIC_READ_WRITE_LENS = new ReadWriteLens<Payment, String>() {
+        @Override
+        public final String get(Payment object) {
+            if (object == null) {
+                return null;
+            }
+            Receiver receiver = object.getReceiver();
+            if (receiver == null) {
+                return null;
+            }
+            Bank bank = receiver.getBank();
+            if (bank == null) {
+                return null;
+            }
+            return bank.getBic();
+        }
 
-    public static final ReadLens<Payment, String> PAYMENT_PAYER_BANK_BIC_READ_LENS = Lenses.readLens(Payment::getPayer)
-            .andThen(Lenses.readLens(Payer::getBank))
-            .andThen(Lenses.readLens(Bank::getBic));
+        @Override
+        public final void set(Payment object, String property) {
+            if (object == null) {
+                return;
+            }
+            Receiver receiver = object.getReceiver();
+            if (receiver == null) {
+                return;
+            }
+            Bank bank = receiver.getBank();
+            if (bank == null) {
+                return;
+            }
+            bank.setBic(property);
+        }
+    };
 
-    public static final ReadWriteLens<Payment, String> PAYMENT_PAYER_BANK_BIC_READ_WRITE_LENS = Lenses.readLens(Payment::getPayer)
-            .andThen(Lenses.readLens(Payer::getBank))
-            .andThen(Lenses.readWriteLens(Bank::getBic, Bank::setBic));
+    public static final ReadLens<Payment, String> PAYMENT_PAYER_BANK_BIC_READ_LENS = new ReadLens<Payment, String>() {
+        @Override
+        public final String get(Payment object) {
+            if (object == null) {
+                return null;
+            }
+            Payer payer = object.getPayer();
+            if (payer == null) {
+                return null;
+            }
+            Bank bank = payer.getBank();
+            if (bank == null) {
+                return null;
+            }
+            return bank.getBic();
+        }
+    };
+
+    public static final ReadWriteLens<Payment, String> PAYMENT_PAYER_BANK_BIC_READ_WRITE_LENS = new ReadWriteLens<Payment, String>() {
+        @Override
+        public final String get(Payment object) {
+            if (object == null) {
+                return null;
+            }
+            Payer payer = object.getPayer();
+            if (payer == null) {
+                return null;
+            }
+            Bank bank = payer.getBank();
+            if (bank == null) {
+                return null;
+            }
+            return bank.getBic();
+        }
+
+        @Override
+        public final void set(Payment object, String property) {
+            if (object == null) {
+                return;
+            }
+            Payer payer = object.getPayer();
+            if (payer == null) {
+                return;
+            }
+            Bank bank = payer.getBank();
+            if (bank == null) {
+                return;
+            }
+            bank.setBic(property);
+        }
+    };
 
     private PaymentLenses() {
         throw new UnsupportedOperationException("Can not create instance of factory class");
