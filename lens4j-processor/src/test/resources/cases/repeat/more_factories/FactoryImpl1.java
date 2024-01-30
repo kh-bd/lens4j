a9011c490578ee20bef812275bdcb8e81fc262fa
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-01-30 10:47:53 +0300

    Add test for more than one factory.
    (cherry picked from commit 4a734743ee6dd6d33671f30a90c70ecbf695e695)

---
+++ lens4j-processor/src/test/resources/cases/repeat/more_factories/FactoryImpl1.java
@@ -0,0 +1,25 @@
+package cases.repeat.more_factories;
+
+import dev.khbd.lens4j.core.ReadLens;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.UnsupportedOperationException;
+import javax.annotation.processing.Generated;
+
+@Generated("dev.khbd.lens4j.processor.LensProcessor")
+public final class FactoryImpl1 {
+    public static final ReadLens<Customer, String> CUSTOMER_NAME1 = new ReadLens<Customer, String>() {
+        @Override
+        public final String get(Customer object) {
+            if (object == null) {
+                return null;
+            }
+            return object.getName();
+        }
+
+    };
+
+    private FactoryImpl1() {
+        throw new UnsupportedOperationException("Can not create instance of factory class");
+    }
+}
