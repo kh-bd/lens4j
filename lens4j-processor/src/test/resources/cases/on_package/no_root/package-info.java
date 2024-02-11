53a1569b526de2081cfdf988b0088fa6008025d8
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-02-11 12:48:54 +0300

    GenLenses is allowed on package level.
    (cherry picked from commit e90a2ff063804a328b75e5b511d6531ea1835409)

---
+++ lens4j-processor/src/test/resources/cases/on_package/no_root/package-info.java
@@ -0,0 +1,6 @@
+@GenLenses(lenses = @Lens(path = "id"), factoryName = "Factory.java")
+package cases.on_package.no_root;
+
+import dev.khbd.lens4j.core.annotations.GenLenses;
+import dev.khbd.lens4j.core.annotations.Lens;
+
